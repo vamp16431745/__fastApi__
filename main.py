647fc1de8198eb79a53396358725e5f0223201b7
啤酒肚恩巴 <79435806+vamp16431745@users.noreply.github.com>
2023-06-27 06:23:34 +0000

    item_id

--- main.py
+++ main.py
@@ -8,3 +8,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Jonathan"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
+
