cf35a753b14878aaf6000b9ef71ce09f85c84fe6
啤酒肚恩巴 <79435806+vamp16431745@users.noreply.github.com>
2023-06-27 07:02:37 +0000

    傳出時間 光線 溫度

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
-import math
-from datetime import datetime
 from fastapi import FastAPI
-
+from datetime import datetime
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Jonathan"}
+def read_root():
+    return {"Hello": "Jonathan"}
 
 @app.get("/items/{item_id}")
 async def read_item(item_id):
     return {"item_id": item_id}
 
-@app.get("/raspberry/")
-async def read_item(time: datetime = datetime.now(), light:float=0.0, temperature:float=0.0):
+#query parameter
+@app.get("/raspberry")
+async def read_item(time:datetime = datetime.now(),light: float = 0.0, temperature: float = 0.0):
     return {
         "時間":time.strftime("%Y%m%d %H:%M:%S"),
         "光線":light,
         "溫度":temperature
-
     }
-
