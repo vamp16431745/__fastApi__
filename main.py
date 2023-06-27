f50feb88600a92c4a4e16a9cb7d975b2042c8906
啤酒肚恩巴 <79435806+vamp16431745@users.noreply.github.com>
2023-06-27 08:30:36 +0000

    導入MySQL

--- main.py
+++ main.py
@@ -1,7 +1,43 @@
 from fastapi import FastAPI
 from datetime import datetime
+import sqlite3
+from sqlite3 import Error
+
 app = FastAPI()
 
+def create_connection(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+
+    return conn
+
+def create_table(conn):
+    sql_tasks = """
+    CREATE TABLE IF NOT EXISTS iot1(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        date TEXT NOT NULL,
+        light REAL NOT NULL,
+        temperature REAL NOT NULL
+    );
+    """
+
+    try:
+        cursor = conn.cursor()
+        cursor.execute(sql_tasks)
+    except:
+        print("error")
+
+def insert_project(conn, project):
+    sql = """
+    INSERT INTO iot1(date,light,temperature)
+    VALUES(?,?,?)
+    """
+    cursor = conn.cursor()
+    cursor.execute(sql,project)
+    conn.commit()
 
 @app.get("/")
 def read_root():
@@ -14,7 +50,10 @@ async def read_item(item_id):
 #query parameter
 @app.get("/raspberry")
 async def read_item(time:str = datetime.now().strftime("%Y%m%d %H:%M:%S"),light: float = 0.0, temperature: float = 0.0):
-    
+    conn = create_connection('data.db')
+    create_table(conn)
+    insert_project(conn, (time,light,temperature))
+    conn.close()
     return {
         "Time":time,
         "Light":light,
